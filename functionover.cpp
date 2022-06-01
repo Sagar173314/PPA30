0803f400e9b682fef21fb25e984abf14642e7461
Sagar Mahajan <105958095+Sagar173314@users.noreply.github.com>
2022-06-01 11:12:23 +0530

    Add files via upload

---
+++ functionover.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+ussing namespace std;
+
+public:
+
+in addition(int n01, int no2)
+{
+    int ans = 0;
+    ans = no1 + no2;
+    return ans;
+}
+int Addition(int no1, int no2, int no3)    
+{
+    int ans = 0
+    ans = no1 + no2 + no3;
+    return ans;
+}
+int Addition(int no1, int no2, int no3, int no4)
+{
+    int ans = 0;
+    
+}
---
+++ oop.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+class Arithematic
+{
+    public:
+    //characteristics
+    int iNo1;
+    int iNo2;
+
+    //Behaviour
+
+    Arithematic()           //Default Constructor
+    {
+        cout<<"Inside default cnstructor\n";
+        iNo1 = 0;
+        iNo2 = 0; 
+
+    }
+    
+        Arithematic(int A, int B)  //Parameterised constructor
+        {
+            cout<<"Inside parameterised constructor\n";
+            iNo1 = A;
+            iNo2 = B;
+        }
+
+        ~Arithematic()    //Destuctor
+        {
+            cout<<"Inside Destructor\n";
+            
+        }
+
+    int Addition()
+    {
+        int iAns = 0;
+        iAns = iNo1 + iNo2;
+        return iAns;
+    }
+
+    int Substraction()
+    {
+        int iAns = 0;
+        iAns = iNo1 - iNo2;
+        return iAns;
+    }
+};
+
+int main()
+{
+    auto iValue1 = 0,iValue2 = 0,iRet = 0;
+
+    cout<<"Enter first number\n";
+    cin>>iValue1;               //11
+
+    cout<<"Enter second number\n";
+    cin>>iValue2;               //10
+
+    Arithematic obj1;      //Default Constructor
+    Arithematic obj2(iValue1, iValue2);    //Parameterised constructor
+
+    iRet = obj2.Addition();
+    cout<<"Addition  is : "<<iRet<<"\n";
+
+    iRet = obj2.Substraction();
+    cout<<"Substraction is : "<<iRet<<"\n";
+
+    return 0;
+}
